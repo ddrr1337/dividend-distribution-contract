1a24e1f88fbc79193eb33975c3a286669e50e4b9
inc <incaelum@hotmail.com>
2024-02-14 17:41:13 +0100

    first commit

---
+++ DividendERC20.sol
@@ -0,0 +1,130 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+contract DividendERC20 is ERC20, Ownable {
+    address public activeTokenAddress;
+
+    /////////////DIVIDND VARIABLES////////////////////
+    struct UserAccounts {
+        uint256 unclaimedBalance;
+        uint256 lastDividendPoints;
+    }
+    mapping(address => UserAccounts) public userAccounts; // reward token address => to user Address => to UserAccounts data
+    uint256 public pointMultipliyer = 10 ** 18;
+    uint256 public totalDividendPoints;
+
+
+
+    constructor(
+         address _activeTokenAddress
+        ) ERC20("Dividend Token", "DIV") {
+        activeTokenAddress = _activeTokenAddress;
+    
+    }
+
+    function mint(address account, uint256 amount) public {
+
+        _mint(account,amount);
+    }
+
+
+    ///////////////////////////DIVIDENDS//////////////////////////
+    function _dividendsOwing(address account) internal view returns (uint256) {
+        uint256 newDividendPoints = totalDividendPoints -
+            userAccounts[account].lastDividendPoints;
+        uint256 owning = (balanceOf(account) * newDividendPoints) /
+            pointMultipliyer;
+
+        return owning;
+    }
+
+    function transfer(
+        address to,
+        uint256 amount
+    ) public virtual override returns (bool) {
+        _updateDividends(msg.sender, to);
+        _transfer(msg.sender, to, amount);
+        return true;
+    }
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) public virtual override returns (bool) {
+        address spender = _msgSender();
+        _spendAllowance(from, spender, amount);
+        _updateDividends(from, to);
+        _transfer(from, to, amount);
+        return true;
+    }
+
+    function _updateDividends(address from, address to) internal {
+        uint256 owingFrom = _dividendsOwing(from);
+        uint256 owingTo = _dividendsOwing(to);
+
+        if (owingFrom > 0) {
+            userAccounts[from]
+                .unclaimedBalance += owingFrom;
+            userAccounts[from]
+                .lastDividendPoints = totalDividendPoints;
+        }
+        if (owingTo > 0) {
+            userAccounts[to].unclaimedBalance += owingTo;
+            userAccounts[to]
+                .lastDividendPoints = totalDividendPoints;
+        }
+        if (balanceOf(to) == 0) {
+            userAccounts[to].unclaimedBalance += owingTo;
+            userAccounts[to]
+                .lastDividendPoints = totalDividendPoints;
+        }
+    }
+
+    /////////////////CLAIM AND CHECK REWARDS /////////////////////
+
+    function claimRewards() public {
+        uint256 owing = _dividendsOwing(msg.sender);
+
+        userAccounts[msg.sender].unclaimedBalance += owing;
+        userAccounts[msg.sender]
+            .lastDividendPoints = totalDividendPoints;
+        uint256 userUnClaimedBalance = userAccounts[msg.sender]
+            .unclaimedBalance;
+        userAccounts[msg.sender].unclaimedBalance = 0;
+        ERC20(activeTokenAddress).transfer(msg.sender, userUnClaimedBalance);
+    }
+
+    function checkRewards(
+        address user
+    ) public view returns (uint256) {
+        uint256 owing = _dividendsOwing(user);
+        uint256 unClaimed = userAccounts[user].unclaimedBalance +
+            owing;
+        return unClaimed;
+    }
+
+    ///////////////////////DEPOSIT DIVIDENDS////////////////////////
+    function depositDividends(
+        uint256 amount
+    ) external onlyOwner {
+        require(
+            ERC20(activeTokenAddress).transferFrom(
+                msg.sender,
+                address(this),
+                amount
+            ),
+            "TokenReward transfer failed"
+        );
+       
+        // NO SE PUEDE LLAMAR A ESTA FUNCION SI totalASupply() == 0
+        totalDividendPoints += ((amount *
+            pointMultipliyer) / totalSupply());
+
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,33 @@
+# Dividend Distribution Contract
+
+## Use Case:
+
+Let's suppose you are the owner of an ERC20 contract. Say you want the tokens of that ERC20 contract to represent a share of your company's profits. Now, occasionally, you want to distribute those profits, let's say in (DAI), to the holders of your ERC20 token.
+
+This contract attempts to solve the following problems that arise from dividend distribution:
+
+1. We could use a `deposit()` function that receives the DAI, and then we could iterate through all accounts holding our token balance, calculate the proportional share for each user, and create a mapping `address => amountReward` so that the user can later claim that `amountReward`.
+
+   However, doing it this way would be catastrophic:
+   - When the owner calls `deposit()`, they would have to update the entire mapping of `address => amountReward`. This would either result in extremely high gas costs for the contract owner when calling `deposit()` or, in the worst case, the network would return a "max gas exceeded" error, preventing the deposit.
+
+2. We could focus on another system:
+   Declare a variable called `dividends` that stores the amount contributed in the `deposit()` function and a `claimRewards()` function for users to call to retrieve their corresponding share and subtract the claimed value from the `dividends` variable. Then update a mapping to ensure that the user cannot claim again.
+
+   However, this would also generate a serious problem, as a user could claim dividends, then send their ERC20 tokens to another wallet, and claim dividends again by calling `claimRewards()`.
+
+The solution proposed in this contract:
+
+When the owner calls `deposit()`, a variable called `totalDividendPoints` will be updated. This variable serves as a reference for dividend distribution.
+
+The contract also overrides the `transfer` and `transferFrom` functions so that each time user balances are updated, the amount of dividends they can claim is also updated.
+
+This approach avoids the for loop that could drive up gas costs for the owner and also mitigates the issue of users claiming dividends, as a user can no longer switch wallets and claim again.
+
+I have also added a useful function called `checkRewards()` for the frontend to calculate the dividends that correspond to a user, allowing the users to see theinr rewards before claiming.
+
+## Dividend Distribution Contract Functions:
+
+- `depositDividends(uint256 amount)`: Allows the owner to deposit dividends. Users can then claim their share.
+- `claimRewards()`: Users can call this function to claim their dividends.
+- `checkRewards(address user)`: Provides a way for the frontend to calculate the dividends that belong to a user.
